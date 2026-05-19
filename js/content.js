00c929fd847311341241af9f68a517bbd2637ba5
jbryan06 <test@example.com>
2026-05-19 09:50:53 +0800

    ..

--- js/content.js
+++ js/content.js
@@ -253,7 +253,7 @@ const setWeddingDate = (weddingDate) => {
 document.addEventListener("DOMContentLoaded", async () => {
   const weddingData = await getWeddingData();
   // image cache version
-  recacheImages("51820261");
+  recacheImages("5192026");
 
   setWeddingCouple(weddingData.groom, weddingData.bride);
   setCeremonyOfficial(weddingData.officiant);
