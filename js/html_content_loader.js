c405550364fabec5890fc95597022341e64289c8
jbryan06 <test@example.com>
2026-07-01 11:41:30 +0800

    ..

--- js/html_content_loader.js
+++ js/html_content_loader.js
@@ -3,7 +3,7 @@ const CONTENT_BUILD_VERSION = "6.30.26" //reache all
 //Assets
 const ICON_VERSION = "1.3.0";
 const FONT_VERSION = "1.3.0";
-const STYLE_VERSION = "1.4.0";
+const STYLE_VERSION = "1.5.0";
 
 //HTML Fragments
 const HTML_HEADER_VERSION = "1.3.0";
